ee6b1004581ce8c783cff3281513d07fc80319c4
Panasak <89185433+Panasak@users.noreply.github.com>
2021-12-31 09:27:23 +0100

    trying

---
+++ model building/model_building.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file
+
+"""
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\tango\Documents\GitHub\Deep-learning-placeholder\title_basics.csv")
+
+df = df.drop(['tconst','primaryTitle','originalTitle','endYear','runtimeMinutes'],axis=1)
+
+df = df[df['startYear'] != '\\N']
+
+df['genres'].apply(lambda x : x.split(','))
